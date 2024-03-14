846cd8b5e6187ee7f997df3a299cfaeacb2ca5e0
Rajendra <rajagokhale@hotmail.com>
2024-03-14 18:04:28 +0530

    Commit Students.java file.

---
+++ Students.java
@@ -0,0 +1,15 @@
+public class Students
+{
+	//data_type variable_name = value;
+	//variable types in Java => instance or non-static, static or class, local
+	int rollNo = 100;
+	String first_name = "John";
+	int age = 15;
+	double marks = 78.765;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Roll No = " + rollNo);
+	}
+
+}
